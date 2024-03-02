8a29ce927cbb09fecdd31964caffe06558c4393b
XXNNDDRR <xndrgo@gmail.com>
2024-03-02 22:28:25 +0700

    Initial Commit

---
+++ TL/TLApp.swift
@@ -0,0 +1,17 @@
+//
+//  TLApp.swift
+//  TL
+//
+//  Created by Alexander on 02.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
